a9050a8624d29b70f4c4be36965f262557f5a885
Kumar D Anand <anandkumarmd@gmail.com>
2024-09-16 19:35:07 -0400

    spring azure demo app

---
+++ src/main/java/com/kda/azure/demo/KdaSbAzureDemoApplication.java
@@ -0,0 +1,21 @@
+package com.kda.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class KdaSbAzureDemoApplication {
+
+
+	@GetMapping("/hello")
+	public String hellworld() {
+		return "Hello from kda azure !!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdaSbAzureDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kda/azure/demo/KdaSbAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kda.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdaSbAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
